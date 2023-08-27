1a7a3bb80af3c67fdcdee834818d50c1736a286d
Patricia Naomi <patricianaomiyama@gmail.com>
2023-08-26 23:44:05 -0300

    inicio

---
+++ ex001.java
@@ -0,0 +1,20 @@
+public class ex001 {
+
+    /* Ex 001 - Escreva uma classe para ler o nome de um aluno e calcular a média final
+    considerando que a mesma será composta por 4 notas: n1, n2, n3 e n4. Ao final, o programa
+    deve imprimir o nome do aluno e a média final calculada.
+        1. Identifique os dados de entrada
+        2. Qual será o processamento realizado
+        3. Quais são os dados de saída
+     */
+    
+    public static void main(String[] args) {
+        int num = 1;
+        var nome = "Pati";
+
+        System.out.println(num);
+        System.out.println("Oie");
+        System.out.println("Oie");
+    }
+
+}
---
+++ ex002.java
@@ -0,0 +1,5 @@
+public class ex002 {
+    public static void main(String[] args) {
+        
+    }
+}
